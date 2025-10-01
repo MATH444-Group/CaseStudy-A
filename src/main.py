304d2cd47b97c52d67535b7ba74eb8099fc5d398
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-09-30 22:29:13 -0700

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import logging
+import os
+import pandas as pd
+import sys
+
+if not os.path.isdir('utils'):
+  os.system('git clone https://github.com/GevChalikyan/utils.git')
+else:
+  os.system('''cd utils
+    git pull
+    cd ..''')
+
+sys.path.append('utils')
+import data_loader as dl
+
+
+
+
+
+def main():
+
+  file_path = '../data/tests_data.csv'
+  
+  df = dl.load_df(file_path)
+
+  print(df.head)
+
+
+
+if __name__ == '__main__':
+
+  import logger
+  logger.setup()
+
+  main()
