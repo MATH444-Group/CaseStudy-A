c4209d9db57823d6e0446253f6ccba80b3b5ccf2
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-10-02 21:57:04 -0700

    Recording Results in Output File
    Also now recording a more detailed p-value and r-squared

--- src/main.py
+++ src/main.py
@@ -40,6 +40,10 @@ def main():
   data_file_path = '../data/tests_data.csv'
   output_regression_file_path = '../images/regression_line.png'
   output_regression_dir_path = '../images'
+  output_analysis_file_path = '../docs/results.txt'
+  output_analysis_dir_path = '../docs'
+
+
 
   df = dl.load_df(data_file_path)
 
@@ -59,7 +63,15 @@ def main():
   plt.legend()
   plt.grid(True)
 
-  print(results.summary())
+  p_value = results.f_pvalue
+  r_squared = results.rsquared
+
+  if not os.path.isdir(output_analysis_dir_path):
+    os.mkdir(output_analysis_dir_path)
+  with open(output_analysis_file_path, 'w') as file:
+    file.write(results.summary().as_text())
+    file.write(f'\n\nFull P-Value: {p_value}')
+    file.write(f'\nFull R-Squared: {r_squared}')
 
   if not os.path.isdir(output_regression_dir_path):
     os.mkdir(output_regression_dir_path)
